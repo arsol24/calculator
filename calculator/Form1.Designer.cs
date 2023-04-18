d8d585586b99d4ebfc4a127659e91327f063e96f
User 14 <User 14@RINI>
2023-04-18 08:43:11 +0200

    Add project files.

---
+++ calculator/Form1.Designer.cs
@@ -0,0 +1,477 @@
+﻿namespace calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button21 = new Button();
+            button22 = new Button();
+            button23 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonHighlight;
+            button1.Location = new Point(12, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 48);
+            button1.TabIndex = 0;
+            button1.Text = "%";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(81, 180);
+            button2.Name = "button2";
+            button2.Size = new Size(63, 48);
+            button2.TabIndex = 1;
+            button2.Text = "CE";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonHighlight;
+            button3.Location = new Point(150, 180);
+            button3.Name = "button3";
+            button3.Size = new Size(63, 48);
+            button3.TabIndex = 2;
+            button3.Text = "C";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = SystemColors.ButtonHighlight;
+            button4.Location = new Point(218, 180);
+            button4.Name = "button4";
+            button4.Size = new Size(63, 48);
+            button4.TabIndex = 3;
+            button4.Text = "<=";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Transparent;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button5.ForeColor = SystemColors.ButtonHighlight;
+            button5.Location = new Point(12, 234);
+            button5.Name = "button5";
+            button5.Size = new Size(63, 48);
+            button5.TabIndex = 4;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Transparent;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button6.ForeColor = Color.Transparent;
+            button6.Location = new Point(81, 234);
+            button6.Name = "button6";
+            button6.Size = new Size(63, 48);
+            button6.TabIndex = 5;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.Transparent;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.ForeColor = Color.Transparent;
+            button7.Location = new Point(150, 234);
+            button7.Name = "button7";
+            button7.Size = new Size(63, 48);
+            button7.TabIndex = 6;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.Transparent;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button8.ForeColor = Color.Transparent;
+            button8.Location = new Point(219, 234);
+            button8.Name = "button8";
+            button8.Size = new Size(63, 48);
+            button8.TabIndex = 7;
+            button8.Text = "/";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Transparent;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button9.ForeColor = Color.Transparent;
+            button9.Location = new Point(12, 288);
+            button9.Name = "button9";
+            button9.Size = new Size(63, 48);
+            button9.TabIndex = 8;
+            button9.Text = "7";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Transparent;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button10.ForeColor = Color.Transparent;
+            button10.Location = new Point(81, 288);
+            button10.Name = "button10";
+            button10.Size = new Size(63, 48);
+            button10.TabIndex = 9;
+            button10.Text = "8";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Transparent;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button11.ForeColor = Color.Transparent;
+            button11.Location = new Point(150, 288);
+            button11.Name = "button11";
+            button11.Size = new Size(63, 48);
+            button11.TabIndex = 10;
+            button11.Text = "9";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Transparent;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button12.ForeColor = Color.Transparent;
+            button12.Location = new Point(219, 288);
+            button12.Name = "button12";
+            button12.Size = new Size(63, 48);
+            button12.TabIndex = 11;
+            button12.Text = "x";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Transparent;
+            button13.FlatStyle = FlatStyle.Flat;
+            button13.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button13.ForeColor = Color.Transparent;
+            button13.Location = new Point(12, 342);
+            button13.Name = "button13";
+            button13.Size = new Size(63, 48);
+            button13.TabIndex = 12;
+            button13.Text = "4";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.Transparent;
+            button14.FlatStyle = FlatStyle.Flat;
+            button14.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button14.ForeColor = Color.Transparent;
+            button14.Location = new Point(81, 342);
+            button14.Name = "button14";
+            button14.Size = new Size(63, 48);
+            button14.TabIndex = 13;
+            button14.Text = "5";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Transparent;
+            button15.FlatStyle = FlatStyle.Flat;
+            button15.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button15.ForeColor = Color.Transparent;
+            button15.Location = new Point(150, 342);
+            button15.Name = "button15";
+            button15.Size = new Size(63, 48);
+            button15.TabIndex = 14;
+            button15.Text = "6";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Transparent;
+            button16.FlatStyle = FlatStyle.Flat;
+            button16.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button16.ForeColor = Color.Transparent;
+            button16.Location = new Point(219, 342);
+            button16.Name = "button16";
+            button16.Size = new Size(63, 48);
+            button16.TabIndex = 15;
+            button16.Text = "-";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button17
+            // 
+            button17.BackColor = Color.Transparent;
+            button17.FlatStyle = FlatStyle.Flat;
+            button17.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button17.ForeColor = Color.Transparent;
+            button17.Location = new Point(12, 396);
+            button17.Name = "button17";
+            button17.Size = new Size(63, 48);
+            button17.TabIndex = 16;
+            button17.Text = "1";
+            button17.UseVisualStyleBackColor = false;
+            // 
+            // button18
+            // 
+            button18.BackColor = Color.Transparent;
+            button18.FlatStyle = FlatStyle.Flat;
+            button18.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button18.ForeColor = Color.Transparent;
+            button18.Location = new Point(81, 396);
+            button18.Name = "button18";
+            button18.Size = new Size(63, 48);
+            button18.TabIndex = 17;
+            button18.Text = "2";
+            button18.UseVisualStyleBackColor = false;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.Transparent;
+            button19.FlatStyle = FlatStyle.Flat;
+            button19.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button19.ForeColor = Color.Transparent;
+            button19.Location = new Point(150, 396);
+            button19.Name = "button19";
+            button19.Size = new Size(63, 48);
+            button19.TabIndex = 18;
+            button19.Text = "3";
+            button19.UseVisualStyleBackColor = false;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.Transparent;
+            button20.FlatStyle = FlatStyle.Flat;
+            button20.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button20.ForeColor = Color.Transparent;
+            button20.Location = new Point(219, 396);
+            button20.Name = "button20";
+            button20.Size = new Size(63, 48);
+            button20.TabIndex = 19;
+            button20.Text = "+";
+            button20.UseVisualStyleBackColor = false;
+            button20.Click += button20_Click;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = SystemColors.ButtonHighlight;
+            label1.Location = new Point(12, 130);
+            label1.Name = "label1";
+            label1.Size = new Size(270, 35);
+            label1.TabIndex = 20;
+            label1.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.BorderStyle = BorderStyle.Fixed3D;
+            label2.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(12, 95);
+            label2.Name = "label2";
+            label2.Size = new Size(132, 35);
+            label2.TabIndex = 21;
+            label2.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // label3
+            // 
+            label3.BackColor = Color.Transparent;
+            label3.BorderStyle = BorderStyle.Fixed3D;
+            label3.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(150, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(132, 35);
+            label3.TabIndex = 22;
+            label3.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Transparent;
+            label4.BorderStyle = BorderStyle.Fixed3D;
+            label4.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(12, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(270, 75);
+            label4.TabIndex = 23;
+            label4.TextAlign = ContentAlignment.BottomRight;
+            // 
+            // button21
+            // 
+            button21.BackColor = Color.Transparent;
+            button21.FlatStyle = FlatStyle.Flat;
+            button21.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button21.ForeColor = Color.Transparent;
+            button21.Location = new Point(12, 450);
+            button21.Name = "button21";
+            button21.Size = new Size(63, 48);
+            button21.TabIndex = 24;
+            button21.Text = "1";
+            button21.UseVisualStyleBackColor = false;
+            // 
+            // button22
+            // 
+            button22.BackColor = Color.Transparent;
+            button22.FlatStyle = FlatStyle.Flat;
+            button22.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button22.ForeColor = Color.Transparent;
+            button22.Location = new Point(81, 450);
+            button22.Name = "button22";
+            button22.Size = new Size(63, 48);
+            button22.TabIndex = 25;
+            button22.Text = "1";
+            button22.UseVisualStyleBackColor = false;
+            // 
+            // button23
+            // 
+            button23.BackColor = Color.Transparent;
+            button23.FlatStyle = FlatStyle.Flat;
+            button23.Font = new Font("Rubik", 8.999999F, FontStyle.Bold, GraphicsUnit.Point);
+            button23.ForeColor = Color.Transparent;
+            button23.Location = new Point(150, 450);
+            button23.Name = "button23";
+            button23.Size = new Size(63, 48);
+            button23.TabIndex = 26;
+            button23.Text = "1";
+            button23.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Gray;
+            BackgroundImage = Properties.Resources._8275340;
+            ClientSize = new Size(294, 522);
+            Controls.Add(button23);
+            Controls.Add(button22);
+            Controls.Add(button21);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button20);
+            Controls.Add(button19);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Button button21;
+        private Button button22;
+        private Button button23;
+    }
+}
---
+++ calculator/Form1.cs
@@ -0,0 +1,15 @@
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
